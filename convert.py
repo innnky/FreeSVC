c8fbd22a33dc986eee7bc51c0ebfe73b7ebe05d0
innnky <xiyiji508@gmail.com>
2022-12-05 16:25:31 +0800

    inference

--- convert.py
+++ convert.py
@@ -18,12 +18,39 @@ import logging
 import parselmouth
 import numpy as np
 
-def get_f0(path,p_len=None, f0_up_key=0):
+
+def stft(y):
+    return librosa.stft(
+        y=y,
+        n_fft=1280,
+        hop_length=160,
+        win_length=1280,
+    )
+
+
+def energy(y):
+    # Extract energy
+    S = librosa.magphase(stft(y))[0]
+    e = np.sqrt(np.sum(S ** 2, axis=0))  # np.linalg.norm(S, axis=0)
+    return e.squeeze()  # (Number of frames) => (654,)
+
+
+def get_energy(path, p_len=None):
+    wav, sr = librosa.load(path, 16000)
+    e = energy(wav)
+    if p_len is None:
+        p_len = wav.shape[0] // 160
+    assert e.shape[0] - p_len < 2, (e.shape[0], p_len)
+    e = e[: p_len]
+    return e
+
+
+def get_f0(path, p_len=None, f0_up_key=0):
     x, _ = librosa.load(path, 16000)
     if p_len is None:
-        p_len = x.shape[0]//160
+        p_len = x.shape[0] // 160
     else:
-        assert abs(p_len-x.shape[0]//160) < 2, (path, p_len, x.shape)
+        assert abs(p_len - x.shape[0] // 160) < 2, (path, p_len, x.shape)
     time_step = 160 / 16000 * 1000
     f0_min = 50
     f0_max = 1100
@@ -34,9 +61,9 @@ def get_f0(path,p_len=None, f0_up_key=0):
         time_step=time_step / 1000, voicing_threshold=0.6,
         pitch_floor=f0_min, pitch_ceiling=f0_max).selected_array['frequency']
 
-    pad_size=(p_len - len(f0) + 1) // 2
-    if(pad_size>0 or p_len - len(f0) - pad_size>0):
-        f0 = np.pad(f0,[[pad_size,p_len - len(f0) - pad_size]], mode='constant')
+    pad_size = (p_len - len(f0) + 1) // 2
+    if (pad_size > 0 or p_len - len(f0) - pad_size > 0):
+        f0 = np.pad(f0, [[pad_size, p_len - len(f0) - pad_size]], mode='constant')
 
     f0bak = f0.copy()
     f0 *= pow(2, f0_up_key / 12)
@@ -47,12 +74,13 @@ def get_f0(path,p_len=None, f0_up_key=0):
     f0_coarse = np.rint(f0_mel).astype(np.int)
     return f0_coarse, f0bak
 
+
 logging.getLogger('numba').setLevel(logging.WARNING)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--hpfile", type=str, default="configs/freevc.json", help="path to json config file")
-    parser.add_argument("--ptfile", type=str, default="logs/freevc/G_130000.pth", help="path to pth file")
+    parser.add_argument("--ptfile", type=str, default="logs/freevc/G_14000.pth", help="path to pth file")
     parser.add_argument("--outdir", type=str, default="output", help="path to output dir")
     parser.add_argument("--use_timestamp", default=False, action="store_true")
     args = parser.parse_args()
@@ -70,36 +98,41 @@ if __name__ == "__main__":
     _ = utils.load_checkpoint(args.ptfile, net_g, None)
 
     print("Loading WavLM for content...")
-    cmodel = utils.get_cmodel(0)
+    cmodel = utils.get_hubert_model(0)
 
     print("Processing text...")
-    titles, srcs, tgts, shifts = [], [], [], []
+    titles, srcs, tgts, pshifts, eshifts = [], [], [], [], []
 
-    for sample in os.listdir("sample"):
-        for i in range(3):
-            for shift in (-12,-6,0,6,12):
-                title = f"{sample[:-4]}-{i}-{shift}"
-                src = f"sample/{sample}"
-                tgt = i
-                titles.append(title)
-                srcs.append(src)
-                tgts.append(tgt)
-                shifts.append(shift)
+    for line in open("convert.txt").readlines():
+        sample, i, pshift, eshift = line.strip().split("|")
+        title = f"{sample[:-4]}-{i}-{pshift}-{eshift}"
+        src = f"sample/{sample}"
+        tgt = int(i)
+        titles.append(title)
+        srcs.append(src)
+        tgts.append(tgt)
+
+        pshifts.append(int(pshift))
+        eshifts.append(float(eshift))
 
     print("Synthesizing...")
     with torch.no_grad():
-        for line in tqdm(zip(titles, srcs, tgts, shifts)):
-            title, src, tgt, shift = line
+        for line in tqdm(zip(titles, srcs, tgts, pshifts, eshifts)):
+            title, src, tgt, pshift, eshift = line
             # src
             wav_src, _ = librosa.load(src, sr=16000)
             wav_src = torch.from_numpy(wav_src).unsqueeze(0).cuda()
-            c = utils.get_content(cmodel, wav_src)
+            c = utils.get_hubert_content(cmodel, wav_src)
             c = torch.repeat_interleave(c, repeats=2, dim=2)
-            print(c.shape)
+            # print(c.shape)
             g = torch.LongTensor([[tgt]]).cuda()
-            cf0, f0bk = get_f0(src, c.shape[-1], f0_up_key=shift)
+            cf0, f0bk = get_f0(src, c.shape[-1], f0_up_key=pshift)
             f0 = torch.LongTensor(cf0).unsqueeze(0).cuda()
-            audio = net_g.infer(c,f0=f0, g=g)
+
+            e = get_energy(src, c.shape[-1]) * eshift
+            e = torch.LongTensor(e).unsqueeze(0).cuda()
+
+            audio = net_g.infer(c, f0=f0, energy=e, g=g)
             audio = audio[0][0].data.cpu().float().numpy()
             if args.use_timestamp:
                 timestamp = time.strftime("%m-%d_%H-%M", time.localtime())
